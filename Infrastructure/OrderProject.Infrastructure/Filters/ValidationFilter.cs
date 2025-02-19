918cf3a34b5f779b6012047fcbe2046fb2382edb
Goncanur Uz <goncanur.uz@servisplan.com.tr>
2025-02-19 08:35:34 +0300

    validation error base response

--- Infrastructure/OrderProject.Infrastructure/Filters/ValidationFilter.cs
+++ Infrastructure/OrderProject.Infrastructure/Filters/ValidationFilter.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using OrderProject.Application.DTOs;
+using OrderProject.Application.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +21,9 @@ namespace OrderProject.Infrastructure.Filters
                         e => e.Key,
                         e => e.Value.Errors.Select(err => err.ErrorMessage));
 
-                context.Result = new BadRequestObjectResult(errors);
+                var resp = new BaseResponse();
+                resp.SetError("",errors);
+                context.Result = new BadRequestObjectResult(resp);
                 return;
             }
             await next();
